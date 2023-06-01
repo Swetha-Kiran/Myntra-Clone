046b9d84ac6d3f9d80dea3eb1634541e893d13c7
Swetha Reddy <swethakiranmv@gmail.com>
2023-06-01 18:02:57 +0530

    ecommerce

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+//menu
+var menuitems = document.getElementById("menu-items");
+menuitems.style.maxHeight = "0px";
 
+var menu = document.querySelector(".menu-icon");
+
+menu.addEventListener("click", () => {
+  if (menuitems.style.maxHeight == "0px") {
+    menuitems.style.maxHeight = "200px"
+  }
+  else {
+    menuitems.style.maxHeight = "0px";
+  }
+})
 //form js
 
 var login = document.getElementById("LoginForm");
@@ -6,12 +19,12 @@ var register = document.getElementById("RegForm");
 var indicator = document.getElementById("Indicator");
 
 
-document.getElementById("login-id").addEventListener("click",()=>{
+document.getElementById("login-id").addEventListener("click", () => {
   register.style.transform = "translateX(300px)";
   login.style.transform = "translateX(300px)";
   indicator.style.transform = "translateX(0px)";
 });
-document.getElementById("register-id").addEventListener("click",()=>{
+document.getElementById("register-id").addEventListener("click", () => {
   register.style.transform = "translateX(0px)";
   login.style.transform = "translateX(0px)";
   indicator.style.transform = "translateX(100px)";
